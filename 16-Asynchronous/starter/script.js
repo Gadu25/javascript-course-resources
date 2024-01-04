b72ad31648cc0828629cf8e758a7f1c9f97d64da
Alex <alxanderudag@gmail.com>
2024-01-05 05:57:42 +0800

    callback hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,6 +6,20 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 // OLD SCHOOL
+const renderCountry = function(data, className = '') {
+    const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}M</p>
+            </div>
+        </article>
+        `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
 
 const getCountryData = function(country) {
     const request = new XMLHttpRequest();
@@ -16,21 +30,25 @@ const getCountryData = function(country) {
         const data = JSON.parse(this.responseText)[0]
         console.log(data)
 
-        const html = `
-        <article class="country">
-            <img class="country__img" src="${data.flags.png}" />
-            <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}M</p>
-            </div>
-        </article>
-        `;
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
+        renderCountry(data)
+
+        // get neighbour country
+        const [neighbour] = data.borders
+
+        if(!neighbour) return
+
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request2.send();
+
+        request2.addEventListener('load', function(){
+            const data = JSON.parse(this.responseText)[0]
+    
+            renderCountry(data, 'neighbour')
+        })
     })
 }
 
-getCountryData('philippines')
-getCountryData('australia')
-getCountryData('japan')
+getCountryData('portugal')
+// getCountryData('australia')
+// getCountryData('japan')
